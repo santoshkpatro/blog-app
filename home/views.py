f5630e01f70e1a4c6b856983ac93c7067e806b32
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2020-03-31 21:10:23 +0530

    Basic structure design complete

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('You are visiting home page')
+
+
+def contact(request):
+    return HttpResponse('You are visiting contact page')
+
+
+def about(request):
+    return HttpResponse('You are visiting about page')
